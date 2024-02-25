fa6f439d0c16de40477166b7d622ef283c663b10
GuillermoInfiesta <ginfiestap@gmail.com>
2024-02-25 20:49:44 +0100

    Web practica 1, v1.0

--- Practica_1/routes/index.tsx
+++ Practica_1/routes/index.tsx
@@ -4,23 +4,10 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-rainbow">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-        <text>Para buscar la leccion añade /{"[numero]"} a la url actual</text>
-      </div>
+    <div class="index comic-sans">
+      No tan rápido, introduce el id de la frase que quieres en la url de la
+      forma https://practica-1.deno.dev/id<br />
+      O pulsa <a href="/1">aqui</a> para ver la primera frase<br />
     </div>
   );
 }
